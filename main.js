c8599d1a361cc49d17e5ed5f9867d2de1b51b212
milderz <mzunax@gmail.com>
2023-11-29 11:07:22 -0600

    update main.js

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+// Create a new ball element
+const ball = document.createElement("div");
+let ballXPosition = 160;
+
+function renderBall() {
+  const box = document.querySelector(".box");
+
+  ball.classList.add("ball");
+
+  ball.style.left = `${ballXPosition}px`;
+  ball.style.top = "-40px";
+
+  // Append the ball to the box
+  box.appendChild(ball);
+}
+
+renderBall();
+
+function moveBallLeft() {
+  if (ballXPosition + 10 >= 20) {
+    ball.style.left = `${(ballXPosition -= 10)}px`;
+  }
+}
+
+function moveBallRight() {
+  if (ballXPosition + 10 <= 330) {
+    ballXPosition += 10;
+    ball.style.left = `${ballXPosition}px`;
+  }
+}
 function dropBall() {
   const box = document.querySelector(".box");
 
@@ -9,7 +39,7 @@ function dropBall() {
   const x = Math.random() * (box.offsetWidth - 20);
 
   // Set the initial position of the ball
-  ball.style.left = `${x}px`;
+  ball.style.left = `${ballXPosition}px`;
   ball.style.top = "0px";
 
   // Append the ball to the box
@@ -21,14 +51,14 @@ function dropBall() {
 
 function animateBall(ball) {
   let posY = 0;
-  const fallSpeed = 2;
+  const fallSpeed = 10;
 
   function fall() {
     posY += fallSpeed;
     ball.style.top = `${posY}px`;
 
     // Check if the ball reaches the bottom of the box
-    if (posY + ball.offsetHeight < 600) {
+    if (posY + ball.offsetHeight < 580) {
       requestAnimationFrame(fall);
     }
   }
@@ -41,5 +71,9 @@ function animateBall(ball) {
 window.addEventListener("keydown", function (event) {
   if (event.code === "Space") {
     dropBall();
+  } else if (event.code === "ArrowLeft") {
+    moveBallLeft();
+  } else if (event.code === "ArrowRight") {
+    moveBallRight();
   }
 });
